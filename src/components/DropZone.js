26976cdc704501a9639d4973924466b184c72cd3
Josh Bryson <josh@bryson.me.uk>
2019-07-18 09:50:42 +0100

    Minor styling update and file acceptance experiment

--- src/components/DropZone.js
+++ src/components/DropZone.js
@@ -1,30 +1,20 @@
-import React, {useCallback, useEffect, useState} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, {useEffect, useState} from 'react';
+import {useDropzone} from 'react-dropzone';
 
 function DropZone() {
     const [imageFiles, setImageFiles] = useState([]);
+    const maxSize = 5048576;
 
-    // const onDrop = useCallback(acceptedFiles => {
-    //     const reader = new FileReader()
-
-    //     reader.onabort = () => console.log('file reading was aborted')
-    //     reader.onerror = () => console.log('file reading has failed')
-    //     reader.onload = () => {
-    //     // Do whatever you want with the file contents
-    //     const binaryStr = reader.result
-    //     console.log(binaryStr)
-    //     }
-
-    //     acceptedFiles.forEach(file => reader.readAsBinaryString(file))
-    // }, [])
-
-    const {acceptedFiles, getRootProps, getInputProps, isDragActive, isDragReject} = useDropzone({
-        accept: 'image/*, .pdf, .doc',
+    const {acceptedFiles, getRootProps, getInputProps, isDragActive, isDragReject, rejectedFiles
+    } = useDropzone({
+        accept: 'application/pdf, image/*, text/plain',
         onDrop: acceptedFiles => {
             setImageFiles(acceptedFiles.map(file => Object.assign(file, {
               preview: URL.createObjectURL(file)
             })));
-        }
+        },
+        minSize: 0,
+        maxSize,
     });
 
     const files = acceptedFiles.map(file => (
@@ -50,8 +40,10 @@ function DropZone() {
 
     useEffect(() => () => {
         // Make sure to revoke the data uris to avoid memory leaks
-        files.forEach(file => URL.revokeObjectURL(file.preview));
-    }, [files]);
+        acceptedFiles.forEach(file => URL.revokeObjectURL(file.preview));
+    }, [acceptedFiles]);
+
+    const isFileTooLarge = rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
 
     return (
         <React.Fragment>
@@ -60,11 +52,12 @@ function DropZone() {
                     <input {...getInputProps()} className="dropzone-input"/>
                     {!isDragActive && 'Drag \'n\' Drop Some Files Here, or Click to Select Files'}
                     {isDragActive && !isDragReject && "Drop Your Files Here..."}
-                    {isDragReject && "File Type Not Accepted, Sorry!"}
+                    {isDragActive && isDragReject && "File Type Not Accepted, Sorry!"}
+                    {isFileTooLarge && "File is Too Large, Sorry!"}
                 </div>
                 <div className="dropzone-filelist">
                     <hr />
-                    <h4>Files</h4>
+                    <h4>File List</h4>
                     <ul>{files}</ul>
                 </div>
                 <div className="dropzone-filelist">
